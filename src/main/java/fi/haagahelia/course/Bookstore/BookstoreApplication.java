d6723e73b25bcae9da1b05170dbd22b70149c0bc
juliaict <julia.jarson2@myy.haaga-helia.fi>
2019-02-26 23:12:28 +0200

    C4.1

--- src/main/java/fi/haagahelia/course/Bookstore/BookstoreApplication.java
+++ src/main/java/fi/haagahelia/course/Bookstore/BookstoreApplication.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Bean;
 
 import fi.haagahelia.course.Bookstore.domain.Book;
 import fi.haagahelia.course.Bookstore.domain.BookRepository;
+import fi.haagahelia.course.Bookstore.domain.Category;
+import fi.haagahelia.course.Bookstore.domain.CategoryRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
@@ -18,12 +20,17 @@ public class BookstoreApplication {
 		SpringApplication.run(BookstoreApplication.class, args);
 	}
 	@Bean
-	public CommandLineRunner bookDemo(BookRepository repository) {
+	public CommandLineRunner bookDemo(BookRepository repository, CategoryRepository crepository) {
 		return (args) -> {
 			
 			log.info("save a couple of books");
-			repository.save(new Book("Tilinpäätösanalyysi", "Juha-Pekka Kallunk", 2014, "978-952-1-42195-2", 58.70));
-			repository.save(new Book("Yritystutkimuksen tilinpäätösana", "Yritystutkimus ry", 2017, "978-952-4-95427-3", 27));	
+			crepository.save(new Category("Kirjanpito"));
+			crepository.save(new Category("Tilintarkastus"));
+			crepository.save(new Category("Verotus"));
+			
+			repository.save(new Book("Tilinpäätösanalyysi", "Juha-Pekka Kallunk", 2014, "978-952-1-42195-2", 58.70, crepository.findByName("Kirjanpito").get(0)));
+			repository.save(new Book("Yritystutkimuksen tilinpäätösana", "Yritystutkimus ry", 2017, "978-952-4-95427-3", 27, crepository.findByName("Tilintarkastus").get(0)));
+			repository.save(new Book("Yritystutkimuksen tilinpäätösana", "Yritystutkimus ry", 2017, "978-952-4-95427-3", 27, crepository.findByName("Verotus").get(0)));
 			
 			log.info("fetch all books");
 			for (Book book : repository.findAll()) {
--- src/main/java/fi/haagahelia/course/Bookstore/web/BookController.java
+++ src/main/java/fi/haagahelia/course/Bookstore/web/BookController.java
@@ -1,17 +1,59 @@
 package fi.haagahelia.course.Bookstore.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import fi.haagahelia.course.Bookstore.domain.Book;
+import fi.haagahelia.course.Bookstore.domain.BookRepository;
+import fi.haagahelia.course.Bookstore.domain.CategoryRepository;
 
 
 
 @Controller
 
 public class BookController {
+	@Autowired
+	private BookRepository repository; 
+	
+	@Autowired
+	private CategoryRepository crepository;
+	
+	
 	@RequestMapping(value = "/index", method = RequestMethod.GET)
 	public String getNewBookForm(Model model) {
 				return "index";//linkki index.html
 	}
+	@RequestMapping(value="/booklist")
+    public String bookList(Model model) {	
+        model.addAttribute("books", repository.findAll());
+        return "booklist";//linkki booklist.html
+    }
+	@RequestMapping(value = "/add")
+    public String addBook(Model model){
+    	model.addAttribute("book", new Book());
+    	model.addAttribute("categories", crepository.findAll());
+        return "addbook";//linkki addbook.html
+    }
+	
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+    public String save(Book book){
+        repository.save(book);
+        return "redirect:booklist";
+    }
+	
+	@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+    public String deleteBook(@PathVariable("id") Long bookId, Model model) {
+    	repository.deleteById(bookId);
+        return "redirect:../booklist";
+    }   
+
+	@RequestMapping(value = "/edit/{id}", method = RequestMethod.GET)
+    public String editBook(@PathVariable("id") Long bookId, Model model) {
+    	model.addAttribute("book", repository.findById(bookId));
+    	return "editbook";
+    }
 }
