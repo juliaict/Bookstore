5a39cb44dc3767f3fea67ed57dd954db0d4bb3ee
juliaict <julia.jarson2@myy.haaga-helia.fi>
2019-03-11 23:26:18 +0200

    C6.2 Security userDetailService

--- src/main/java/fi/haagahelia/course/Bookstore/BookstoreApplication.java
+++ src/main/java/fi/haagahelia/course/Bookstore/BookstoreApplication.java
@@ -11,6 +11,8 @@ import fi.haagahelia.course.Bookstore.domain.Book;
 import fi.haagahelia.course.Bookstore.domain.BookRepository;
 import fi.haagahelia.course.Bookstore.domain.Category;
 import fi.haagahelia.course.Bookstore.domain.CategoryRepository;
+import fi.haagahelia.course.Bookstore.domain.User;
+import fi.haagahelia.course.Bookstore.domain.UserRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
@@ -20,7 +22,7 @@ public class BookstoreApplication {
 		SpringApplication.run(BookstoreApplication.class, args);
 	}
 	@Bean
-	public CommandLineRunner bookDemo(BookRepository repository, CategoryRepository crepository) {
+	public CommandLineRunner bookDemo(BookRepository repository, CategoryRepository crepository, UserRepository urepository) {
 		return (args) -> {
 			
 			log.info("save a couple of books");
@@ -31,6 +33,16 @@ public class BookstoreApplication {
 			repository.save(new Book("Tilinpäätösanalyysi", "Juha-Pekka Kallunk", 2014, "978-952-1-42195-2", 58.70, crepository.findByName("Kirjanpito").get(0)));
 			repository.save(new Book("Yritystutkimuksen tilinpäätösana", "Yritystutkimus ry", 2017, "978-952-4-95427-3", 27, crepository.findByName("Tilintarkastus").get(0)));
 			repository.save(new Book("Yritystutkimuksen tilinpäätösana", "Yritystutkimus ry", 2017, "978-952-4-95427-3", 27, null));
+			
+			//Luo käyttäjiä: admin/adminadmin user/useruser
+			
+			User user1 = new User("user", "$2a$10$Hkalfpe8PygrqR3WacL.B.plDXQkHoeqgAVjnZ59p1B3pzYEehoES", "user@bookstore.com", "USER");
+			User user2 = new User("admin", "$2a$10$dg.BpwRAc8o4viUPDopmbuzYswkWTJhVGtiOt6HNKM8omanynQEdq", "ADMIN");
+			urepository.save(user1);
+			urepository.save(user2);
+										
+						
+			
 			
 			log.info("fetch all books");
 			for (Book book : repository.findAll()) {
